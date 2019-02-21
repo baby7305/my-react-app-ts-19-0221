c4fc889873ab2bd028deb357b420f41226673a98
baby7305 <songhaibei7305@163.com>
2019-02-21 22:36:40 +0800

    复杂组件

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,43 @@ class App extends Component {
   }
 }
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-const element = <Welcome name="Sara" />;
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+const element = <Comment
+  date={comment.date}
+  text={comment.text}
+  author={comment.author} />;
 
 export default App;
