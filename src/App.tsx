73555e48cc8c93ad094dbb6a7af245fbe02a2705
baby7305 <songhaibei7305@163.com>
2019-02-21 19:02:45 +0800

    引入对象，方法传参需要开启隐式推断

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,19 @@ class App extends Component {
   }
 }
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 export default App;
