d283a84a99ab9400db0f744d34a3ab997e357523
baby7305 <songhaibei7305@163.com>
2019-02-21 14:46:03 +0800

    const调用H5元素块

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
+      element
     );
   }
 }
+
+const element = <h1>Hello, world!</h1>;
 
 export default App;
