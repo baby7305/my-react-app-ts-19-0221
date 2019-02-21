0c44a3b3f67eb9feade203bf84b435d4b78906ea
baby7305 <songhaibei7305@163.com>
2019-02-21 23:17:28 +0800

    元素作为组件，分解

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function tick() {
-    const element = (
+function Clock(props) {
+    return (
         <div>
             <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
         </div>
     );
-    ReactDOM.render(element, document.getElementById('root'));
+}
+
+function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')
+    );
 }
 
 setInterval(tick, 1000);
