48fc00e2568231fa922df34375871bf7487122a8
baby7305 <songhaibei7305@163.com>
2019-03-02 21:46:05 +0800

    渲染多个组件

---
+++ src/components/listItems/listItems.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const numbers = [1, 2, 3, 4, 5];
+
+const listItems = numbers.map((numbers) =>
+    <li>{numbers}</li>
+);
+
+export default listItems;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import listItems from './components/listItems/listItems';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ul>{listItems}</ul>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
