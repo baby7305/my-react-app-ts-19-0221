57422204f0b3d6e62a2789aa5a67dfdab065a46d
baby7305 <songhaibei7305@163.com>
2019-03-02 18:32:41 +0800

    把WelcomeDialog作为Dialog的特殊实例

---
+++ src/components/WelcomeDialog/Dialog.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import './Dialog.css';
+
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+        </FancyBorder>
+    );
+}
+
+export default Dialog;
--- src/components/WelcomeDialog/WelcomeDialog.tsx
+++ src/components/WelcomeDialog/WelcomeDialog.tsx
@@ -1,24 +1,11 @@
 import * as React from 'react';
-import './WelcomDialog.css';
-
-function FancyBorder(props) {
-    return (
-        <div className={'FancyBorder FancyBorder-' + props.color}>
-            {props.children}
-        </div>
-    );
-}
+import Dialog from './Dialog';
 
 function WelcomeDialog() {
     return (
-        <FancyBorder color="blue">
-            <h1 className="Dialog-title">
-                Welcome
-      </h1>
-            <p className="Dialog-message">
-                Thank you for visiting our spacecraft!
-      </p>
-        </FancyBorder>
+        <Dialog
+            title="Welcome"
+            message="Thank you for visiting our spacecraft!" />
     );
 }
 
