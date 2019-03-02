4ba9be560bbf20996e24654298c38ec1f63aa9e3
baby7305 <songhaibei7305@163.com>
2019-03-02 22:04:47 +0800

    嵌套一个函数，没啥意思

--- src/components/listItems/listItems.tsx
+++ src/components/listItems/listItems.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+function ListItem(props) {
+    // 对啦！这里不需要指定key:
+    return <li>{props.value}</li>;
+}
+
 function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
-        <li key={number.toString()}>
-            {number}
-        </li>
+        // 又对啦！key应该在数组的上下文中被指定
+        <ListItem key={number.toString()}
+            value={number} />
     );
     return (
-        <ul>{listItems}</ul>
+        <ul>
+            {listItems}
+        </ul>
     );
 }
 
