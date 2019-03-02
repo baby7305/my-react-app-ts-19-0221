9f1898f342921e103fde067bf13d74c9cfbd637c
baby7305 <songhaibei7305@163.com>
2019-03-02 21:04:01 +0800

    与运算符&&介绍

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Mailbox from './components/Mailbox/Mailbox';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+ReactDOM.render(
+    <Mailbox unreadMessages={messages} />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
