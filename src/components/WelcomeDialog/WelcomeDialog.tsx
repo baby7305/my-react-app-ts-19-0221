5f5377cda8fe597449913295a3bf09862c2d7480
baby7305 <songhaibei7305@163.com>
2019-03-02 17:52:28 +0800

    包含关系示例

---
+++ src/components/WelcomeDialog/WelcomeDialog.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import './WelcomDialog.css';
+
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                Welcome
+      </h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+      </p>
+        </FancyBorder>
+    );
+}
+
+export default WelcomeDialog;
