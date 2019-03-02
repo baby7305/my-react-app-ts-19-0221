c1ba3d9ee9d72b08acb0095adb763f851697de14
baby7305 <songhaibei7305@163.com>
2019-03-02 22:14:56 +0800

    key不是全局唯一，可以多处使用，互不影响

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Blog from './components/Blog/Blog';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const posts = [
+    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+    { id: 2, title: 'Installation', content: 'You can install React from npm.' }
+];
+
+ReactDOM.render(
+    <Blog posts={posts} />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
