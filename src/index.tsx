e7aa7f346a98f581022f0b4e46f21a52fb84258e
baby7305 <songhaibei7305@163.com>
2019-03-03 16:55:46 +0800

    modal嵌入在parent组件中调用child组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Parent from './components/Modal/Parent';
 import * as serviceWorker from './serviceWorker';
 
 const appRoot = document.getElementById('app-root');
 
-ReactDOM.render(<App />, appRoot);
+ReactDOM.render(<Parent />, appRoot);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
