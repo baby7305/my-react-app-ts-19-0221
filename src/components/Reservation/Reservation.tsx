6d688e760283702e47aeef1aeec2d1e28cd15421
baby7305 <songhaibei7305@163.com>
2019-03-03 13:14:20 +0800

    根据标签名称选择展示组件

---
+++ src/components/Reservation/Reservation.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export interface Props {
+}
+
+interface State {
+    isGoing: boolean;
+    numberOfGuests: number;
+}
+
+class Reservation extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        let partialState = {};
+        partialState[name] = value;
+        this.setState(partialState);
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Is going:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Number of guests:
+                    <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+            </form>
+        );
+    }
+}
+
+export default Reservation;
